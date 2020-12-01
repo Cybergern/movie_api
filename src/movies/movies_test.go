6de10fcab17da609a47e98a48965ff71eeb7bf6f
Stefan Thyberg <thyberg@gmail.com>
2020-12-01 01:58:07 +0100

    Added some more tests for failure states and refactored test functions.

--- src/movies/movies_test.go
+++ src/movies/movies_test.go
@@ -8,24 +8,45 @@ import (
 	"testing"
 )
 
-func TestGetMovies(t *testing.T) {
-	req, err := http.NewRequest("GET", "/movies", nil)
+func makeRequest(method string, headers map[string]string, t *testing.T) *httptest.ResponseRecorder {
+	req, err := http.NewRequest(method, "/movies", nil)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("Error during request: %s", err)
 	}
-	req.Header.Set("X-Secret", "1234")
-	expected, err := ioutil.ReadFile("test_data/movies_expected_data.json")
-	if err != nil {
-		t.Fatalf("Error loading file with expected data: %s", err)
+	for key, value := range headers {
+		req.Header.Set(key, value)
 	}
 	rr := httptest.NewRecorder()
 	finalHandler := http.HandlerFunc(GetMovies)
 	Middleware(finalHandler).ServeHTTP(rr, req)
+	return rr
+}
+
+func TestGetMovies(t *testing.T) {
+	rr := makeRequest("GET", map[string]string{"X-Secret": "1234"}, t)
+	expected, err := ioutil.ReadFile("test_data/movies_expected_data.json")
+	if err != nil {
+		t.Fatalf("Error loading file with expected data: %s", err)
+	}
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
 	if strings.TrimSpace(rr.Body.String()) != string(expected) {
 		t.Errorf("Handler returned unexpected response body: got %v want %v", rr.Body.String(), string(expected))
+	}
+}
+
+func TestMiddleware(t *testing.T) {
+	rr := makeRequest("GET", map[string]string{}, t)
+	if status := rr.Code; status != http.StatusForbidden {
+		t.Errorf("Header missing: got %v want %v", status, http.StatusForbidden)
+	}
+}
+
+func TestGetMoviesWrongMethod(t *testing.T) {
+	rr := makeRequest("POST", map[string]string{"X-Secret": "1234"}, t)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("Wrong method: got %v want %v", status, http.StatusBadRequest)
 	}
 }
